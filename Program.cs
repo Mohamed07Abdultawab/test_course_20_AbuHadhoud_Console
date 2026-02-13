353b95a5ec05309de6453673e83a51bbc1d45433
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-02-13 21:10:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace test_course_20_AbuHadhoud_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
