e48e834f3b30f1061a070d34c41b73817352aacd
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-02-15 18:58:29 +0200

    section6 part3 -> using static directive

---
+++ section6/part3/using static directive.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Math;//import the static members of the Math class
+
+namespace Program
+{
+    //internal class Program
+    //{
+    //    static void Main()
+    //    {
+    //        double number = 16.0;
+    //        double result = Sqrt(number); // Use the Sqrt method directly without Math.Sqrt
+    //        Console.WriteLine($"The square root of {number} is {result}");
+    //    }
+    //}
+}
