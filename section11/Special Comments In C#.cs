a2406befc3e422f86f3c82eb471fb874f2aef080
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-02-22 21:29:08 +0200

    section11 -> Special Comments in C#

---
+++ section11/Special Comments In C#.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_course_20_AbuHadhoud_Console.section11
+{
+    /// <summary>
+    /// This class represents a simple calculator 
+    /// </summary>
+    public class Special_Comments_In_C_
+    {
+        /// <summary>
+        /// Adds two numbers and returns the result.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+
+        /// <summary>
+        /// Subtracts the second number from the first number and reutns the result.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
